ccd495f924a12b121165c4aa202d85a967f92087
cristinachirvas <chirvas.cristina@gmail.com>
2023-09-12 20:58:18 +0300

    Added variables and comments task

---
+++ src/main/java/org/chirvascristina/variablesandcomments/VariabilesAndComments.java
@@ -0,0 +1,25 @@
+package org.chirvascristina.variablesandcomments;
+
+public class VariabilesAndComments {
+    public static void main(String[] args) {
+//        Declare variable primitive type
+        boolean variableBoolean;
+        char variableChar;
+        byte variableByte;
+        short variableShort;
+        int variableInt;
+        long variableLong;
+        float variableFloat;
+        double variableDouble;
+
+//        Initialize the variables
+        variableBoolean = true;
+        variableChar = 'C';
+        variableByte = 35;
+        variableShort = -32700;
+        variableInt = 1988;
+        variableLong = 123456789101112L;
+        variableDouble = -1.7E+300;
+    }
+
+}
